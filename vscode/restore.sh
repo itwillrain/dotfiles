73aaa298a643637f529c3b8379219eee76a5d2ba
takeshi_matsushita <matsushita@neopa.jp>
2020-01-13 17:49:39 +0900

    add vscode files

---
+++ vscode/restore.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+
+if [ -e "$VSCODE_SETTING_DIR/settings.json" ]; then
+  rm "$VSCODE_SETTING_DIR/settings.json"
+  ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+fi
+
+if [ -e "$VSCODE_SETTING_DIR/keybindings.json" ]; then
+  rm "$VSCODE_SETTING_DIR/keybindings.json"
+  ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+fi
+
+cat ./extensions | while read line
+do
+  code --install-extension $line
+done
+code --list-extensions > extensions
