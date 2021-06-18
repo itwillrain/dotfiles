492ed8fca1f8543b799abfbca121b565c6991935
Matsushita Takeshi <matsushita@neopa.jp>
2021-06-18 21:24:46 +0900

    :+1:          機能改善 (fix/feat) update dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+is_dir() {
+    path="$1"
+    [ -d "$path" ]
+}
+
+is_file() {
+    path="$1"
+    [ -f "$path" ]
+}
+
+ensure_dir() {
+    path="$1"
+    if ! is_dir "$path"; then
+        mkdir -p "$path"
+    fi
+}
 
 log() {
     message=$1
     echo 📌 "$message"
 }
+
 ### メモ
 #さきにApp storeにログインしておく
 #link.shを実行しておく
@@ -34,4 +51,21 @@ log "homebrew周りの設定が終了しました"
 ### Brewfileに記載されているアプリをインストール
 log "Brewfileに記載されているアプリをインストールします"
 brew bundle --verbose --file 'Brewfile'
+
+# Stow commandを利用するため、homebrew installを先に行ってください。
+STOW_PACKAGES_PATH=~/dotfiles/packages
+
+# Stow packages直下をsymboliclink
+stow -vd "$STOW_PACKAGES_PATH"  -t ~ $(ls $STOW_PACKAGES_PATH)
+log "symbolic linkを貼りました〜。"
+
+limelight_path=/usr/local/bin/limelight
+if ! is_file "$limelight_path"; then
+    git clone https://github.com/koekeishiya/limelight
+    cd limelight
+    make
+    mv ./bin/limelight /usr/local/bin/limelight
+    cd ../
+    rm -rf limelight
+fi
 
