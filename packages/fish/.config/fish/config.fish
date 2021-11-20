5c976a9892d546e6cb196bf80f854be0ab17165a
Matsushita Takeshi <matsushitatakeshi@team-lab.com>
2021-11-21 02:51:57 +0900

    :art: remove comment

--- packages/fish/.config/fish/config.fish
+++ packages/fish/.config/fish/config.fish
@@ -9,11 +9,6 @@ set -x PATH $ANDROID_HOME/emulator $PATH
 set -x PATH $ANDROID_HOME/platform-tools $PATH
 set -x PATH $ANDROID_HOME/tools $PATH
 set -x PATH $ANDROID_HOME/tools/bin $PATH
-#peco
-#function fish_user_key_bindings
-#  bind \cr peco_select_history	
-#  bind \c] peco_change_directory # Bind for prco change directory to Ctrl+]
-#end
 
 #  The next line updates PATH for the Google Cloud SDK.
 if [ -f '$HOME/google-cloud-sdk/path.fish.inc' ]; . '$HOME/google-cloud-sdk/path.fish.inc'; end
