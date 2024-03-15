1d632fc67611ec86a2e4b10714f72f45de346d8e
Matsushita Takeshi <matsushitatakeshi@team-lab.com>
2024-03-15 19:46:14 +0900

    :wrench: settings(vscode): 出力先Dir修正

--- vscode/restore.sh
+++ vscode/restore.sh
@@ -39,5 +39,5 @@ if [ "$FLG_S" != "TRUE" ]; then
   cat "$SCRIPT_DIR/extensions" | while read line; do
     code --install-extension $line
   done
-  code --list-extensions >extensions
+  code --list-extensions >$SCRIPT_DIR/extensions
 fi
