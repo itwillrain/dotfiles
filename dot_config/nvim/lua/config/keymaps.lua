338d689b8aa0be01ef47c92b652aee9c6aa4e4b8
Matsushita Takeshi <4383381+itwillrain@users.noreply.github.com>
2026-09-14 04:58:37 +0900

    feat(nvim): LazyVimを導入する
    Neovim設定をLazyVimへ移行し、tree-sitter-cliをmise管理に追加する。

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+-- Keep the insert-mode escape shortcuts from the previous init.vim.
+vim.keymap.set("i", "jj", "<Esc>", { silent = true })
+vim.keymap.set("i", "っj", "<Esc>", { silent = true })
---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,17 @@
+-- LazyVim provides the defaults; keep the preferences from the previous init.vim.
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.list = true
+opt.mouse = "a"
+opt.ignorecase = true
+opt.smartcase = true
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.undofile = true
+opt.updatetime = 300
+opt.clipboard:append("unnamedplus")
+opt.helplang = "ja"
